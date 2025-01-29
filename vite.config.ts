cda82d87d6f6b9b3185bf744bf9d3cdf24ff49a8
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-01-29 22:26:19 +0500

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-color-converter/", // Укажите название вашего репозитория
-})
+  base: "/REACT_my-color-converter/", // Замените my-color-converter на имя вашего репозитория
+});
